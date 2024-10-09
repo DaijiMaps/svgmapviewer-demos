74b0fbfd7cfdb459bd8a1bcc4f4719b223aedaa4
Masao Uebayashi <uebayasi@tombiinc.com>
2024-10-09 15:40:05 +0900

    pyqgis wrapper script

--- pyqgis.sh
+++ pyqgis.sh
@@ -5,21 +5,22 @@
 
 QGIS_PREFIX_PATH="/Applications/QGIS-LTR.app/Contents/MacOS"
 
-bin="${QGIS_PREFIX_PATH}/bin"
+_bin="${QGIS_PREFIX_PATH}/bin"
+_exe="${_bin}/python3.9"
 
-exe="${bin}/python3.9"
+PATH="${_bin}:${PATH}"
 
-p="${QGIS_PREFIX_PATH}/../Resources/python"
-p1=${p}
-p2=${p}/plugins
-p3=${p}/plugins/processing
-PYTHONPATH="${p1}:${p2}:${p3}"
+_p="${QGIS_PREFIX_PATH}/../Resources/python"
+_p1=${_p}
+_p2=${_p}/plugins
+_p3=${_p}/plugins/processing
+PYTHONPATH="${_p1}:${_p2}:${_p3}"
 
 GDAL_DATA="${python}/../gdal"
 
 exec env -i \
-    PATH="${bin}:${PATH}" \
+    PATH="${PATH}" \
     QGIS_PREFIX_PATH="${QGIS_PREFIX_PATH}" \
     PYTHONPATH="${PYTHONPATH}" \
     GDAL_DATA="${GDAL_DATA}" \
-    ${exe} $@
+    ${_exe} $@
