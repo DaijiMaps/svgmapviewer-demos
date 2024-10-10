bce7dcbb7c2d55d553b5d13f2edf2c2087a60097
Masao Uebayashi <uebayasi@tombiinc.com>
2024-10-10 23:08:23 +0900

    checkpoint

---
+++ ShinjukuGyoenNationalPark/src/main-map-data.ts
@@ -0,0 +1,23 @@
+import areas from './data/areas.json'
+import centroids from './data/centroids.json'
+import lines from './data/lines.json'
+import measures from './data/measures.json'
+import multipolygons from './data/multipolygons.json'
+import multilinestrings from './data/multistrings.json'
+import origin from './data/origin.json'
+import points from './data/points.json'
+import viewbox from './data/viewbox.json'
+import { MapData } from '@daijimaps/svgmapviewer'
+
+export const mapData: MapData = {
+  areas,
+  origin,
+  measures,
+  viewbox,
+
+  points,
+  lines,
+  multilinestrings,
+  multipolygons,
+  centroids,
+}
