d4e0f115155d954d46e1ff3d2cf950bcd3578378
Masao Uebayashi <uebayasi@tombiinc.com>
2024-10-17 20:38:17 +0900

    KodomoNoKuni

---
+++ packages/demos/KodomoNoKuni/src/main.ts
@@ -0,0 +1,21 @@
+import { svgmapviewer } from '@daijimaps/svgmapviewer'
+import mapSvg from './assets/kodomomap_2024_mono.svg'
+import { RenderMap } from './map'
+
+svgmapviewer({
+  root: 'root',
+  map: 'map1',
+  href: `${mapSvg}`,
+  width: 595.275,
+  height: 841.89,
+  //origViewBox: { x: 0, y: 0, width: 595.275, height: 841.89 },
+  zoomFactor: 2,
+  renderMap: RenderMap,
+  //renderInfo: RenderInfo,
+  copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
+  //mapData,
+  //mapCoord,
+  //mapHtmlStyle,
+  //mapSymbols,
+  //mapNames,
+})
