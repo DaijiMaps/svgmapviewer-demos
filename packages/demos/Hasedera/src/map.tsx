c9c4dcc05b17932673bc07763054201a16343342
Masao Uebayashi <uebayasi@tombiinc.com>
2024-10-24 21:24:21 +0900

    Hasedera

---
+++ packages/demos/Hasedera/src/map.tsx
@@ -0,0 +1,149 @@
+import { RenderMapProps, svgMapViewerConfig } from '@daijimaps/svgmapviewer'
+import {
+  MapLayer,
+  MapMarkers,
+  MapObjects,
+  MapSymbols,
+  RenderMapLayers,
+  RenderMapMarkers,
+  RenderMapObjects,
+  RenderMapSymbols,
+} from '@daijimaps/svgmapviewer/carto'
+import {
+  benchPath,
+  guidePostPath,
+  infoBoardPath,
+  tree4x8Path,
+} from '@daijimaps/svgmapviewer/carto-objects'
+import { MultiPolygon, PointGeoJSON } from '@daijimaps/svgmapviewer/geo'
+import { V } from '@daijimaps/svgmapviewer/tuple'
+import { conv } from './map-data'
+import './map.css'
+import internals from './data/internals.json'
+
+export const getMapLayers: () => MapLayer[] = () => [
+  {
+    type: 'multipolygon',
+    name: 'area',
+    data: internals.features.map(
+      (f) => f.geometry.coordinates
+    ) as unknown as MultiPolygon[],
+  },
+  {
+    type: 'multipolygon',
+    name: 'forest',
+    filter: (f) =>
+      !!f.properties.landuse?.match(/forest/) ||
+      !!f.properties.natural?.match(/wood/),
+  },
+  {
+    type: 'multipolygon',
+    name: 'water',
+    filter: (f) => !!f.properties.natural?.match(/^water$/),
+  },
+  {
+    type: 'line',
+    name: 'ditch',
+    filter: (f) => !!f.properties.waterway?.match(/^(ditch)$/),
+  },
+  {
+    type: 'line',
+    name: 'stream',
+    filter: (f) => !!f.properties.waterway?.match(/^(stream)$/),
+  },
+  {
+    type: 'multipolygon',
+    name: 'building',
+    filter: (f) => !!f.properties.building?.match(/./),
+  },
+  {
+    type: 'multipolygon',
+    name: 'pedestrian-area',
+    filter: (f) => !!f.properties.other_tags?.match(/"pedestrian"/),
+  },
+  {
+    type: 'line',
+    name: 'footway',
+    filter: (f) =>
+      !!f.properties.highway?.match(/^(footway|path|pedestrian|steps)$/),
+  },
+  {
+    type: 'line',
+    name: 'service',
+    filter: (f) => !!f.properties.highway?.match(/^(service)$/),
+  },
+  {
+    type: 'line',
+    name: 'road',
+    filter: (f) =>
+      !!f.properties.highway?.match(/./) &&
+      !f.properties.highway?.match(/^(footway|path|pedestrian|steps|cycleway|service)$/),
+  },
+  {
+    type: 'line',
+    name: 'hedge',
+    filter: (f) => !!f.properties.barrier?.match(/^(hedge)$/),
+  },
+  {
+    type: 'line',
+    name: 'wall',
+    filter: (f) => !!f.properties.barrier?.match(/^(wall|fence)$/),
+  },
+  {
+    type: 'line',
+    name: 'retaining-wall',
+    filter: (f) => !!f.properties.barrier?.match(/^(retaining_wall)$/),
+  },
+]
+
+export const getMapObjects: () => MapObjects[] = () => [
+  {
+    name: 'benches',
+    path: benchPath,
+    width: 0.05,
+    pointsFilter: (f) => !!f.properties.other_tags?.match(/"bench"/),
+  },
+  {
+    name: 'guide-posts',
+    path: guidePostPath,
+    width: 0.05,
+    pointsFilter: (f) => !!f.properties.other_tags?.match(/"guidepost"/),
+  },
+  {
+    name: 'info-boards',
+    path: infoBoardPath,
+    width: 0.05,
+    pointsFilter: (f) =>
+      !!f.properties.other_tags?.match(/"information"=>"(board|map)"/),
+  },
+  {
+    name: 'trees1',
+    path: tree4x8Path,
+    width: 0.15,
+    pointsFilter: (f) => !!f.properties.other_tags?.match(/"tree"/),
+  },
+]
+
+export const getMapSymbols: () => MapSymbols[] = () => [
+  {
+    name: 'toilets',
+    href: '#XToilets',
+    pointsFilter: (f) => !!f.properties.other_tags?.match(/"toilets"/),
+    centroidsFilter: (f) =>
+      !!f.properties.other_tags?.match(/"toilets"/) ||
+      f.properties.amenity === 'toilets',
+  },
+  {
+    name: 'parkings',
+    href: '#XParking',
+    pointsFilter: (f) => !!f.properties.other_tags?.match(/"parking"/),
+    centroidsFilter: (f) => !!f.properties.other_tags?.match(/"parking"/),
+  },
+]
+
+export const getMapMarkers: () => MapMarkers[] = () => [
+  {
+    name: 'all',
+    pointsFilter: (f) => !!f.properties.name?.match(/./),
+  },
+]
