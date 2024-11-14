066016e66a642c83a639c621034e3d1af2098262
Masao Uebayashi <uebayasi@tombiinc.com>
2024-11-15 02:28:20 +0900

    g/c

--- packages/demos/KodomoNoKuni/src/main.ts
+++ packages/demos/KodomoNoKuni/src/main.ts
@@ -9,16 +9,9 @@ svgmapviewer({
   href: `${mapSvg}`,
   width: 580,
   height: 610,
-  //origViewBox: { x: 5, y: 155, width: 580, height: 610 },
   zoomFactor: 2,
   renderMap: RenderMap,
-  //renderInfo: RenderInfo,
   copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
-  //mapData,
-  //mapCoord,
-  //mapHtmlStyle,
-  //mapSymbols,
-  //mapNames,
 })
 
 svgMapViewerConfig.searchCbs.add(workerSearchStart)
