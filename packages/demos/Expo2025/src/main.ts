b86c952cc3f7a31144a8ee2a827100cf7a21b2cd
Masao Uebayashi <uebayasi@tombiinc.com>
2025-04-24 21:41:51 +0900

    Add Expo2025

---
+++ packages/demos/Expo2025/src/main.ts
@@ -0,0 +1,40 @@
+import { svgMapViewerConfig, svgmapviewer } from '@daijimaps/svgmapviewer'
+import {
+  getMapLayers,
+  getMapMarkers,
+  getMapObjects,
+  getMapSymbols,
+} from './map'
+import {
+  mapCoord,
+  mapData,
+  mapHtmlStyle,
+  mapNames,
+  mapSymbols,
+  mapViewBox,
+} from './map-data'
+import { RenderInfo } from './render'
+import { workerSearchStart } from './search'
+
+svgmapviewer({
+  root: 'root',
+  map: 'map1',
+  origViewBox: mapViewBox,
+  zoomFactor: 2,
+  getMapLayers,
+  getMapObjects,
+  getMapSymbols,
+  getMapMarkers,
+  renderInfo: RenderInfo,
+  title: 'Shinjuku-Gyoen Natioan Park',
+  copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
+  mapData,
+  mapCoord,
+  mapHtmlStyle,
+  mapSymbols,
+  mapNames,
+})
+
+svgMapViewerConfig.searchCbs.add(workerSearchStart)
+
+document.title = `svgmapviewer @ ${window.location.host}`
