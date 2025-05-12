910470393f7724d244361d2ec471e2012592d600
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-12 17:10:26 +0900

    pyqgis.sh wrapper for Ubuntu

---
+++ packages/tools/pyqgis-Ubuntu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+#
+# Usage: ./pyqgis.sh extractAreas.py Gumyoji 'name' '弘明寺'
+#
+
+_exe="/usr/bin/python3"
+
+_p="/usr/share/qgis/python"
+_p1=${_p}
+_p2=${_p}/plugins
+_p3=${_p}/plugins/processing
+PYTHONPATH="${_p1}:${_p2}:${_p3}"
+
+exec env \
+    PYTHONPATH="${PYTHONPATH}" \
+    ${_exe} $@
