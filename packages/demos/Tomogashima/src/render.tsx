6972c22326c2c6282c4f51edca0c865f3ecc6dbd
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-14 12:57:56 +0900

    regen

--- packages/demos/Tomogashima/src/render.tsx
+++ packages/demos/Tomogashima/src/render.tsx
@@ -1,3 +1,5 @@
+import { Like } from '@daijimaps/svgmapviewer'
+import { findProperties, getPropertyValue } from '@daijimaps/svgmapviewer/geo'
 import { FacilityInfo, Info, ShopInfo } from './info'
 
 export interface Props {
@@ -5,16 +7,43 @@ export interface Props {
 }
 
 export function RenderInfo(props: Readonly<Props>) {
+  const properties =
+    'address' in props.info.x ? findProperties(props.info.x.address) : null
+  if (properties === null) {
+    return <p>XXX info not found (osm_id={props.info.x.address}) XXX</p>
+  }
+  const props2 = {
+    ...props.info.x,
+    properties,
+  }
   return props.info.x.tag === 'shop'
-    ? RenderShopInfo(props.info.x)
-    : RenderFacilityInfo(props.info.x)
+    ? RenderShopInfo(props2)
+    : RenderFacilityInfo(props2)
 }
 
 function RenderShopInfo(props: Readonly<ShopInfo>) {
+  const website = getPropertyValue(props.properties, 'website')
+  const osm_id = Number(props.properties.osm_id ?? '')
+  const osm_way_id = Number(
+    ('osm_way_id' in props.properties && props.properties.osm_way_id) ?? ''
+  )
+  const id = osm_id !== 0 ? osm_id : osm_way_id !== 0 ? osm_way_id : 0
+
   return (
     <>
-      <p>{props.name}</p>
-      <p>{props.address}</p>
+      <p>
+        {props.properties.name ?? props.name} {id !== 0 && <Like _id={id} />}
+      </p>
+      {website !== null && (
+        <p>
+          website:{' '}
+          <a target="_blank" href={website}>
+            {website}
+          </a>
+        </p>
+      )}
+      {osm_id !== 0 && <p>osm_id: {osm_id}</p>}
+      {osm_way_id !== 0 && <p>osm_way_id: {osm_way_id}</p>}
     </>
   )
 }
@@ -22,8 +51,7 @@ function RenderShopInfo(props: Readonly<ShopInfo>) {
 function RenderFacilityInfo(props: Readonly<FacilityInfo>) {
   return (
     <>
-      <p>{props.name}</p>
-      <p>{props.address}</p>
+      <p>{props.properties.name ?? props.name}</p>
     </>
   )
 }
