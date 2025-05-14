e774221d5d50d8b02901e7834bc1d322d6682f05
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-14 18:52:01 +0900

    add GhibliPark

---
+++ packages/demos/GhibliPark/src/render.tsx
@@ -0,0 +1,68 @@
+import { Like } from '@daijimaps/svgmapviewer'
+import {
+  findProperties,
+  getPropertyValue,
+  OsmPointProperties,
+  OsmPolygonProperties,
+} from '@daijimaps/svgmapviewer/geo'
+import { FacilityInfo, Info, ShopInfo } from './info'
+
+export interface Props {
+  info: Info
+}
+
+export function RenderInfo(props: Readonly<Props>) {
+  const properties =
+    'address' in props.info.x ? findProperties(props.info.x.address) : null
+  if (properties === null) {
+    return <p>XXX info not found (osm_id={props.info.x.address}) XXX</p>
+  }
+  return props.info.x.tag === 'shop'
+    ? RenderShopInfo({ x: props.info.x, properties })
+    : RenderFacilityInfo({ x: props.info.x, properties })
+}
+
+function RenderShopInfo(
+  props: Readonly<{
+    x: ShopInfo
+    properties: OsmPointProperties | OsmPolygonProperties
+  }>
+) {
+  const website = getPropertyValue(props.properties, 'website')
+  const osm_id = Number(props.properties.osm_id ?? '')
+  const osm_way_id = Number(
+    ('osm_way_id' in props.properties && props.properties.osm_way_id) ?? ''
+  )
+  const id = osm_id !== 0 ? osm_id : osm_way_id !== 0 ? osm_way_id : 0
+
+  return (
+    <>
+      <p>
+        {props.properties.name ?? props.x.name} {id !== 0 && <Like _id={id} />}
+      </p>
+      {website !== null && (
+        <p>
+          website:{' '}
+          <a target="_blank" href={website}>
+            {website}
+          </a>
+        </p>
+      )}
+      {osm_id !== 0 && <p>osm_id: {osm_id}</p>}
+      {osm_way_id !== 0 && <p>osm_way_id: {osm_way_id}</p>}
+    </>
+  )
+}
+
+function RenderFacilityInfo(
+  props: Readonly<{
+    x: FacilityInfo
+    properties: OsmPointProperties | OsmPolygonProperties
+  }>
+) {
+  return (
+    <>
+      <p>{props.properties.name ?? props.x.name}</p>
+    </>
+  )
+}
