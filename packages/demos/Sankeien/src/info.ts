41981653f3d6aa0c14000726b1cefab6e37dcfe1
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-14 13:13:16 +0900

    regen Sankeien

--- packages/demos/Sankeien/src/info.ts
+++ packages/demos/Sankeien/src/info.ts
@@ -1,20 +1,29 @@
 import { Info } from '@daijimaps/svgmapviewer'
+import {
+  OsmPointProperties,
+  OsmPolygonProperties,
+} from '@daijimaps/svgmapviewer/geo'
 
 export interface ShopInfo {
   tag: 'shop'
+  properties: OsmPointProperties | OsmPolygonProperties
   name?: string
   address?: string
+  website?: string
 }
 
 export interface FacilityInfo {
   tag: 'facility'
+  properties: OsmPointProperties | OsmPolygonProperties
   name?: string
   address?: string
 }
 
+export type XInfo = ShopInfo | FacilityInfo
+
 declare module '@daijimaps/svgmapviewer' {
   interface Info {
-    x: ShopInfo | FacilityInfo
+    x: XInfo
   }
 }
 
