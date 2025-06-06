a20c92d9b8afe0eeb4447be7e27d63ee1d4d9818
Masao Uebayashi <uebayasi@tombiinc.com>
2025-06-06 17:08:46 +0900

    configs

--- packages/demos/Expo2025/svgmapviewer.config.ts
+++ packages/demos/Expo2025/svgmapviewer.config.ts
@@ -4,6 +4,10 @@ const userConfig: SvgMapViewerConfigUser = {
   title: 'Expo 2025',
   copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
   zoomFactor: 5,
+  cartoConfig: {
+    skipNamePattern: /丁目$|町$|売店$|レストハウス|^新宿御苑$|センター|案内図$|Ticket|シラカシ/,
+    splitNamePattern: /(カフェ|レストラン|ミュージアム|センター|門衛所|御休所|休憩所|案内図|パビリオン|マーケットプレイス|ターミナル|停留所|エクスペリエンス|ポップアップステージ)/,
+  },
 }
 
 document.title = `svgmapviewer @ ${window.location.host}`
--- packages/demos/Yugyoji/svgmapviewer.config.ts
+++ packages/demos/Yugyoji/svgmapviewer.config.ts
@@ -3,7 +3,10 @@ import { SvgMapViewerConfigUser } from '@daijimaps/svgmapviewer'
 const userConfig: SvgMapViewerConfigUser = {
   title: 'Yugyoji',
   copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
-  zoomFactor: 5,
+  zoomFactor: 3,
+  cartoConfig: {
+    skipNamePattern: /学園/,
+  },
 }
 
 document.title = `svgmapviewer @ ${window.location.host}`
