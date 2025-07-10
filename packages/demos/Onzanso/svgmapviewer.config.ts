6f24230c39b20676b9905256442fbf2e4eaf409b
Masao Uebayashi <uebayasi@tombiinc.com>
2025-07-10 12:49:24 +0900

    add Onzanso & Yosuien

---
+++ packages/demos/Onzanso/svgmapviewer.config.ts
@@ -0,0 +1,11 @@
+import { SvgMapViewerConfigUser } from '@daijimaps/svgmapviewer'
+
+const userConfig: SvgMapViewerConfigUser = {
+  title: 'Onzanso',
+  copyright: '@ Daiji Maps | map data @ OpenStreetMap contributers',
+  zoomFactor: 3,
+}
+
+document.title = `svgmapviewer @ ${window.location.host}`
+
+export default userConfig
