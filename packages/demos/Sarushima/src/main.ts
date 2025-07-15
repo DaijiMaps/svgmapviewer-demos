2d40cf5099540447f86dce22e12b17220760cbec
Masao Uebayashi <uebayasi@tombiinc.com>
2025-07-15 10:33:38 +0900

    revert

--- packages/demos/Sarushima/src/main.ts
+++ packages/demos/Sarushima/src/main.ts
@@ -1,7 +1,6 @@
 import { svgmapviewer } from '@daijimaps/svgmapviewer'
 import { renderConfig } from '@daijimaps/svgmapviewer-app'
 import { dataConfig } from './map-data'
-import { renderMapSvg } from './map'
 
 import userConfig from '../svgmapviewer.config'
 
@@ -11,11 +10,4 @@ svgmapviewer({
   ...userConfig,
   root: 'root',
   map: 'map1',
-  renderMap: renderMapSvg,
-  origViewBox: {
-    x: 0,
-    y: 0,
-    width: 1280,
-    height: 800,
-  },
 })
